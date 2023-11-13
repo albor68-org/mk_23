4396903212880ccc6ef9d0a935d62c446525ee4c
Арина Жучкова <liana5818an@gmail.com>
2023-11-13 13:12:28 +0300

    Лабораторная №3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+constexpr uint16_t LEDS{GPIO9 | GPIO13}
+
 int main () {
+    
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDS);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10 | GPIO14);
+
 
     while (true) {
-
+       gpio_toggle(GPIOE, LEDS);
+       for(volatile uint32_t i=0; i< 500'000; i++); 
+       gpio_toggle(GPIOE, GPIO10 | GPIO14);
+       for(volatile uint32_t i=0; i< 500'000; i++); 
+      
     }
 }
