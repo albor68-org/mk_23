66bd21ef97f95dafc384c349c0c5f0c07054ff76
Alexey Ermakov <lexaermak985@gmail.com>
2023-11-13 13:12:15 +0300

    laba3

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+constexpr uint16_t LEDS{GPIO9 | GPIO13}
 int main () {
 
     rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDS);
 
     while (true) {
         
-        gpio_toggle(GPIOE, GPIO9 | GPIO13);
+        gpio_toggle(GPIOE, LEDS);
         for(volatile uint32_t i = 0; i<500000; i++);
     }
 }
