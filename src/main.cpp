97dd7ba85563cfa7072d17192b8aad59bdc75de4
Способова Юлия <sportlove0018@gmail.com>
2023-11-14 18:15:29 +0300

    Добавлена задержка

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,6 @@ int main () {
    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
     while (true) {
 gpio_toggle(GPIOE, GPIO9);
+for (volatile uint32_t i=0; i<500'000; ++i);
     }
 }
