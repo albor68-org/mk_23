7379da681df44fa39c27a957b4f558de1a330207
Shulz Roman <Roma.823@mail.ru>
2023-11-14 18:27:14 +0300

    Разогнан процессор

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-
 int main () {
+
+//Настройка тактовой подсистемы
+rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
 //Настройка порта ввода-вывода
 rcc_periph_clock_enable(RCC_GPIOE);
