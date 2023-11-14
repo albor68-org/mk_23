72b21410f67eb6d6fe3557f8be386f8b17aa517b
Bogdan Zevalov <bzevalov7@gmail.com>
2023-11-14 12:25:56 +0300

    Разгон процессора

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,17 @@
 #include <libopencm3/stm32/gpio.h>
 
 
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
 int main () {
-    
-    rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIO, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
 
-rcc_periph_clock_enable(RCC_GPIOE);
-gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
+
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+    
     while (true) {
         gpio_toggle(GPIOE, GPIO9);
         for(volatile uint32_t i = 0; i < 500'000; ++i);
-        gpio_toggle(GPIOE, GPIO9);
-        for (volatile uint32_t i=0; i<500'000; ++i);
+        
 
     }
 }
