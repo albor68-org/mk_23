c989c24a357a30ba39831366d84969ce13d1666c
Алла Курбатова <Kurbatova630@gmail.com>
2023-11-14 12:25:27 +0300

    Разгон процессора

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 
 int main () {
+
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ])
+
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
 
