f9ba0980638798c6f00dd7ef463478609ba1362b
Alexandr Molodcov <Sanya04012003@gmail.com>
2023-11-14 18:31:18 +0300

    УВЕЛИЧЕНА ТАКТОВАЯ ЧАСТОТА

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 
 int main () {
 
+     // Настройка тактовой подсиситемы
+      rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK-HSI_64MHZ]);
+      
+
     // настройка порта ввода-вывода
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
