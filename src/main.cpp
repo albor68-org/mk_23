9c3ac39e547b9750a5e0f17d6dfb7a154f10d40f
Дима Неклюдов <twonamename301@gmail.com>
2023-11-14 18:07:25 +0300

    Добавлено мигание светодиодом

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
 int main () {
-    while(true) {
 
+    // Настройка порта ввода-вывода
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,  GPIO8 |GPIO9 |GPIO10 |GPIO11 |GPIO12 |GPIO13 | GPIO14 |GPIO15);
+    while(true) {
+    //Переключение светодиода
+      gpio_toggle(GPIOE, GPIO8 |GPIO9 |GPIO10 |GPIO11 |GPIO12 |GPIO13 | GPIO14 |GPIO15);
         
     }
 }
