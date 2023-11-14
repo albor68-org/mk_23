53ccfcda6ecb98522d957be43cc16785af76fc24
Дима Неклюдов <twonamename301@gmail.com>
2023-11-14 18:29:16 +0300

    Увеличена тактовая частота

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <libopencm3/stm32/gpio.h>
 int main () {
 
-    // Настройка порта ввода-вывода
+    // Настройка тактовой подсистемы
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
+
+     // Настройка порта ввода-вывода
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,  GPIO8 |GPIO9 |GPIO10 |GPIO11 |GPIO12 |GPIO13 | GPIO14 |GPIO15);
+    
     while(true) {
     //Переключение светодиода
       gpio_toggle(GPIOE, GPIO8 |GPIO9 |GPIO10 |GPIO11 |GPIO12 |GPIO13 | GPIO14 |GPIO15);
-      for (volatile uint32_t i = 0; i < 300'000; ++i);  
+      for (volatile uint32_t i = 0; i < 500'000; ++i);  
     }
 }
