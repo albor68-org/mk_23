5ff8ca8440ace363d53e2db6f5c6d43ab8582c4e
Алексей Балашов <balashovalex1029@gmail.com>
2023-11-20 15:10:35 +0300

    4LAB

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
+
+constexpr uint16_t LEDS{GPIO9|GPIO13};
 
 int main () {
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
+
 rcc_periph_clock_enable(RCC_GPIOE);
+
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO8);
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO9);
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO10);
@@ -12,25 +18,25 @@ gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO13);
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO14);
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO15);
 
+
+
+timer_set_prescaler(TIM6, 32000-1);
+
+timer_set_period(TIM6, 1000-1);
+
     while (true) {
 
-
-       gpio_toggle(GPIOE, GPIO8);
+if(timer_get_counter(TIM6)<500)
+gpio_set(GPIOE,LEDS);
+else
+gpio_clear(GPIOE,LEDS);
+       /*gpio_set(GPIOE, GPIO8|GPIO12);
        for(volatile uint32_t i=0;i<100'000;++i);
-       gpio_toggle(GPIOE, GPIO9);
-       for(volatile uint32_t i=0;i<150'000;++i);
-       gpio_toggle(GPIOE, GPIO10);
-       for(volatile uint32_t i=0;i<200'000;++i);
-       gpio_toggle(GPIOE, GPIO11);
-       for(volatile uint32_t i=0;i<250'000;++i);
-       gpio_toggle(GPIOE, GPIO12);
-       for(volatile uint32_t i=0;i<300'000;++i);
-       gpio_toggle(GPIOE, GPIO13);
-       for(volatile uint32_t i=0;i<350'000;++i);
-       gpio_toggle(GPIOE, GPIO14);
-       for(volatile uint32_t i=0;i<400'000;++i);
-       gpio_toggle(GPIOE, GPIO15);
-       for(volatile uint32_t i=0;i<450'000;++i);
-
+       gpio_clear(GPIOE, GPIO8|GPIO12);
+       gpio_set(GPIOE, GPIO9|GPIO13);
+       for(volatile uint32_t i=0;i<100'000;++i);
+       gpio_clear(GPIOE, GPIO9|GPIO13);
+       
+*/
     }
 }
