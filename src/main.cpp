eccbf30789cd262c0eff6e28bd40452bcc2017e6
Алексей Балашов <balashovalex1029@gmail.com>
2023-11-23 12:36:45 +0300

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,9 @@
 #include <libopencm3/stm32/timer.h>
 
 constexpr uint16_t LEDS{GPIO9|GPIO13};
+constexpr uint16_t PERIOD_MS{1000};
 
-int main () {
-    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
-
+void setup_LEDS (){
 rcc_periph_clock_enable(RCC_GPIOE);
 
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO8);
@@ -17,26 +16,34 @@ gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO12);
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO13);
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO14);
 gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO15);
+}
 
+void setup_timer (){
+    rcc_periph_clock_enable(RCC_TIM6);
+timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6)/PERIOD_MS-1);
+timer_set_period(TIM6, PERIOD_MS-1);
+timer_enable_counter(TIM6);
+}
 
-
-timer_set_prescaler(TIM6, 32000-1);
-
-timer_set_period(TIM6, 1000-1);
-
-    while (true) {
-
-if(timer_get_counter(TIM6)<500)
+void blink_LEDS (){
+if (timer_get_counter(TIM6)<PERIOD_MS/2)
 gpio_set(GPIOE,LEDS);
 else
 gpio_clear(GPIOE,LEDS);
-       /*gpio_set(GPIOE, GPIO8|GPIO12);
+}
+
+int main () {
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
+setup_LEDS ();
+blink_LEDS ();
+setup_timer ();
+
+while (true) {
+       gpio_set(GPIOE, GPIO8|GPIO12);
        for(volatile uint32_t i=0;i<100'000;++i);
        gpio_clear(GPIOE, GPIO8|GPIO12);
        gpio_set(GPIOE, GPIO9|GPIO13);
        for(volatile uint32_t i=0;i<100'000;++i);
        gpio_clear(GPIOE, GPIO9|GPIO13);
-       
-*/
     }
 }
