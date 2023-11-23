7340856352e7e77beb337f93d8356f7a672ea9a5
Тогулов Алексей <dd626407@gmail.com>
2023-11-23 12:13:51 +0300

    Навели красоту

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,22 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
 
-int main () {
+constexpr uint16_t LEDS8_12{GPIO8 | GPIO12};
+constexpr uint16_t LEDS9_13{GPIO9 | GPIO13};
+constexpr uint16_t LEDS10_14{GPIO10 | GPIO14};
+constexpr uint16_t LEDS11_15{GPIO11 | GPIO15};
 
-
-    //Настройка тактовой подсистемы 
-    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);//умножитель частоты
-    
+void setup_LEDS(){
     //Настройка линий для СИДиодов
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO12);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO13);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10 | GPIO14);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11 | GPIO15);
+}
 
-    //Настройка таймера
+void setup_timer(){
+    //Настройка таймера:
     //1) "Разморозка" таймера
     rcc_periph_clock_enable(RCC_TIM6);
     //2) Настройка делителя
@@ -24,21 +26,32 @@ int main () {
     timer_set_period(TIM6, 1000 - 1);
     //4) Запуск таймера
     timer_enable_counter(TIM6);
+}
 
-    while (true) {
-
-        if(timer_get_counter(TIM6) < 500){
-            //gpio_set(GPIOE, GPIO8 | GPIO12);
-            //gpio_set(GPIOE, GPIO9 | GPIO13);
-            //gpio_set(GPIOE, GPIO10 | GPIO14);
-            gpio_set(GPIOE, GPIO11 | GPIO15);
+void blink_LEDS(){
+    if(timer_get_counter(TIM6) < 500){
+            //gpio_set(GPIOE, LEDS8_12);
+            gpio_set(GPIOE, LEDS9_13);
+            //gpio_set(GPIOE, LEDS10_14);
+            //gpio_set(GPIOE, LEDS11_15);
         }
         else{
-            //gpio_clear(GPIOE, GPIO8 | GPIO12);
-            //gpio_clear(GPIOE, GPIO9 | GPIO13);
-            //gpio_clear(GPIOE, GPIO10 | GPIO14);
-            gpio_clear(GPIOE, GPIO11 | GPIO15);
+            //gpio_clear(GPIOE, LEDS8_12);
+            gpio_clear(GPIOE, LEDS9_13);
+            //gpio_clear(GPIOE, LEDS10_14);
+            //gpio_clear(GPIOE, LEDS11_15);
         }
+}
 
+int main () {
+
+    //Настройка тактовой подсистемы 
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);//умножитель частоты
+    
+    setup_LEDS();
+    setup_timer();
+
+    while (true) {
+        blink_LEDS();
     }
 }
