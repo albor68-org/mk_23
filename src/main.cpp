f372bb47976c77593f74e0fcfb2cfa7853d57be1
Илья Фомин <fomino2328@gmail.com>
2023-11-23 12:13:58 +0300

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,13 @@
 
 constexpr uint16_t LEDS{GPIO10 | GPIO14};
 
-int main () {
-    //Настройка тактовой подсистемы
-    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
-    
-    // Настройка линии для СИД (свето излучающие диоды)
-    rcc_periph_clock_enable(RCC_GPIOE);
+void setup_LEDS () {
+    //Настройка для сид
+   rcc_periph_clock_enable(RCC_GPIOE);
+   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDS);
+}
 
-
-
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO10);
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO14);
-
-
-    //Настройка таймера
+void setup_timer () {
     //"Разморозка" таймера
     rcc_periph_clock_enable(RCC_TIM6);
     //Настройка делителя
@@ -26,21 +19,23 @@ int main () {
     timer_set_period(TIM6, 1000 - 1);
     //Запуск таймера
     timer_enable_counter(TIM6);
+}
 
-    while (true) {
+void blink_LEDS () {
         if (timer_get_counter(TIM6) < 500)
             gpio_set(GPIOE, LEDS);
         else
             gpio_clear(GPIOE, LEDS);
+}
 
-        /*gpio_set(GPIOE, GPIO10 | GPIO14);
-        for (volatile uint32_t i=0; i<50000'000; i++);
-        gpio_clear(GPIOE, GPIO10|GPIO14);
-        gpio_set(GPIOE, GPIO11 | GPIO15);
-        for (volatile uint32_t i=0; i<50000'000; i++);
-        gpio_clear(GPIOE, GPIO11 | GPIO15);*/
+int main () {
+    //Настройка тактовой подсистемы
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
+    setup_LEDS();
+    setup_timer();
 
-    
+    while (true) {
+        blink_LEDS(); 
     }
 }
