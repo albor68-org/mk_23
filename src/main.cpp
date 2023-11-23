2df28ce310e923db5a24ef9edba656ff9b8b83a6
Alexey Ermakov <lexaermak985@gmail.com>
2023-11-23 13:18:05 +0300

    lr5(2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,25 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/cm3/nvic.h>
 constexpr uint16_t LEDS{GPIO9 | GPIO12 | GPIO11 | GPIO10 | GPIO13 | GPIO14 | GPIO15 | GPIO8};
-constexpr uint16_t PERIOD_MS{1000}
+constexpr uint16_t PERIOD_MS{1000};
 void setup_LEDS() {
     //Настройка линий для сид
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDS);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8 | GPIO10 | GPIO12 | GPIO14);
 }
 void setup_timer() {
 //Настройка таймера
     //"Разморозка" таймера
     rcc_periph_clock_enable(RCC_TIM6);
     //Настройка делителя
-    timer_set_prescaler(TIM6, 32000 - 1);
+    timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6) / PERIOD_MS- 1);
     //Указание предела счета
     timer_set_period(TIM6, PERIOD_MS - 1);
+    timer_enable_irq(TIM6, TIM_DIER_UIE);
+    nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
     //Запуск
     timer_enable_counter(TIM6);
 }
@@ -67,4 +71,8 @@ int main() {
         setup_blink();
 
     }
+}
+void tim6_dac_isr() {
+    timer_clear_flag(TIM6, TIM_SR_UIF);
+    gpio_toggle(GPIOE, GPIO8 | GPIO10 | GPIO12 | GPIO14);
 }
