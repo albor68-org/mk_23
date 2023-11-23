4eb24f26e8884b67c5f8aeff7ee4a9d65649aa2b
Илья Фомин <fomino2328@gmail.com>
2023-11-23 12:36:28 +0300

    Refactoring 2

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 
 constexpr uint16_t LEDS{GPIO10 | GPIO14};
 
+constexpr uint16_t PERIOD_MS{1000};
+
 void setup_LEDS () {
     //Настройка для сид
    rcc_periph_clock_enable(RCC_GPIOE);
@@ -14,15 +16,15 @@ void setup_timer () {
     //"Разморозка" таймера
     rcc_periph_clock_enable(RCC_TIM6);
     //Настройка делителя
-    timer_set_prescaler(TIM6, 32000 - 1);
+    timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6) / PERIOD_MS - 1);
     //Указание предела счета
-    timer_set_period(TIM6, 1000 - 1);
+    timer_set_period(TIM6, PERIOD_MS - 1);
     //Запуск таймера
     timer_enable_counter(TIM6);
 }
 
 void blink_LEDS () {
-        if (timer_get_counter(TIM6) < 500)
+        if (timer_get_counter(TIM6) < PERIOD_MS / 2)
             gpio_set(GPIOE, LEDS);
         else
             gpio_clear(GPIOE, LEDS);
