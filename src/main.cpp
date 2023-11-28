72c80456227507e0ecffffa3efecbf25939e688b
Перышкин Антон <anteeck2018@gmail.com>
2023-11-28 13:10:50 +0300

    Лабораторная работа №6

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/timer.h>
 
+#include <libopencm3/cm3/nvic.h>
 
 constexpr uint16_t PERIOD_MS{1000};
 
@@ -12,16 +13,39 @@ void timer_setup() {
     rcc_periph_clock_enable(RCC_TIM6);
     timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6) / PERIOD_MS - 1);
     timer_set_period(TIM6, 1000 - 1);
+    timer_enable_irq(TIM6, TIM_DIER_UIE);
+    nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
 
     timer_enable_counter(TIM6);
 }
+void timer_1_setup() {
+
+    // Настройка таймера
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO14);
+    gpio_set_af(GPIOE, GPIO_AF2, GPIO14);
+
+    rcc_periph_clock_enable(RCC_TIM1);
+
+    timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS - 1);
+    timer_set_period(TIM1, PERIOD_MS - 1);
+
+    timer_set_oc_value(TIM1, TIM_OC4, PERIOD_MS / 2);
+    timer_set_oc_mode(TIM1, TIM_OC4, TIM_OCM_PWM1);
+    timer_enable_oc_output(TIM1, TIM_OC4);
+
+    timer_enable_break_main_output(TIM1);
+
+    timer_enable_counter(TIM1);
+}
+
 
 
 void gpio_setup() {
 
     // Настройка порта ввода-вывода
     rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11);
 }
 
 
@@ -34,6 +58,7 @@ void blink_LED() {
     }
 }
 
+
 int main () {
 
     // Настройка тактов
@@ -43,8 +68,15 @@ int main () {
 
     gpio_setup();
 
+    timer_1_setup();
+
     while (true) {
 
         blink_LED();
     }
+}
+
+void tim6_dac_isr () {
+    timer_clear_flag(TIM6, TIM_SR_UIF);
+    gpio_toggle(GPIOE, GPIO11);
 }
