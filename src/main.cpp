e0071a8245f42cef6dd815fbed1014729dc28a90
Болдин Андрей <aboldin003@gmail.com>
2023-11-28 18:32:53 +0300

    Лабораторная 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,42 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/timer.h>
 
+
+constexpr uint16_t PERIOD_MS(1'000);
+
+
+void blink_LED() {
+        if (timer_get_counter(TIM1) < PERIOD_MS / 2) gpio_set(GPIOE, GPIO9);
+         else gpio_clear(GPIOE, GPIO9);
+}
+
+
+void LED_gpio_setup() {
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11 | GPIO14);
+}
+
+
+void timer_setup () {
+    rcc_periph_clock_enable(RCC_TIM1);
+
+    timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS - 1);
+    timer_set_period(TIM1, PERIOD_MS - 1);
+
+    timer_enable_counter(TIM1);
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
 int main () {
 
-    //Настройка тактовой подсистемы
-    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ] );
 
-    //Настройка порта ввода-вывода
-    rcc_periph_clock_enable(RCC_GPIOE);
-    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9);
-
+    LED_gpio_setup();
+    timer_setup();
 
     while (true) {
-        //Переключение светодиода
-        gpio_toggle(GPIOE, GPIO9);
-        for (volatile uint32_t i = 0; i < 500'000; ++i);
+        blink_LED();     
    }
 }
