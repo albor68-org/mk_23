14a1db66a64a053d8fcfcf61e1a9d05c950d3220
Дима Неклюдов <twonamename301@gmail.com>
2023-11-29 16:26:50 +0300

    ЛР№5

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
 
+#include <libopencm3/cm3/nvic.h>
+
+
 constexpr uint16_t PERIOD_MS{1'000};
 
 
@@ -9,6 +12,8 @@ void blink_LED() {
   if (timer_get_counter(TIM1) < PERIOD_MS / 2) gpio_set(GPIOE, GPIO9 );
   else gpio_clear(GPIOE, GPIO9 );
 }
+
+
 void LED_gpio_setup() {rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT,
      GPIO_PUPD_NONE, GPIO9 | GPIO11 | GPIO14);
@@ -19,8 +24,12 @@ void timer_setup() {
   timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS - 1);
   timer_set_period(TIM1, PERIOD_MS - 1); 
 
+  timer_enable_irq(TIM1, TIM_DIER_UIE);
+  nvic_enable_irq(NVIC_TIM1_UP_TIM16_IRQ);
+
   timer_enable_counter(TIM1);
 }
+ 
 
 //--------------------------------------------
 int main () {
@@ -35,4 +44,10 @@ timer_setup();
     //Переключение светодиода
       blink_LED();
     }
+}
+
+
+void tim1_up_tim16_isr (void) {
+  timer_clear_flag(TIM1, TIM_SR_UIF); 
+  gpio_toggle(GPIOE, GPIO11); 
 }
