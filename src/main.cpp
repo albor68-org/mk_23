a988b8203c5e8c0eb89edd7ccd2b85249af911b7
Shulz Roman <Roma.823@mail.ru>
2023-11-29 16:26:07 +0300

    Лабораторная работа №5

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
 
-
+#include <libopencm3/cm3/nvic.h>
 constexpr uint16_t PERIOD_MS(1'000);
 
 
-void blink_LED(){
+void blink_LED() {
     if (timer_get_counter(TIM1) < PERIOD_MS / 2) gpio_set(GPIOE, GPIO9);
     else gpio_clear(GPIOE, GPIO9);
 }
@@ -24,14 +24,16 @@ void timer_setup () {
     timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS - 1);
     timer_set_period(TIM1, PERIOD_MS - 1);
 
+    timer_enable_irq(TIM1, TIM_DIER_UIE);
+    nvic_enable_irq(NVIC_TIM1_UP_TIM16_IRQ);
+
     timer_enable_counter(TIM1);
 }
 
 
 //-----------------------------------------------------------------------------------
 int main () {
-
-rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
+    rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 
     LED_gpio_setup();
     timer_setup();
@@ -39,4 +41,10 @@ rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
     while (true) {
         blink_LED();
     }
-    }
+}
+
+
+void tim1_up_tim16_isr (void) {
+    timer_clear_flag(TIM1, TIM_SR_UIF);
+    gpio_toggle(GPIOE, GPIO11);
+}
