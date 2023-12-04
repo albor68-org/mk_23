8c5921d3466e29f7cabca70d9e2f19690967ecd2
Илья Фомин <fomino2328@gmail.com>
2023-12-04 15:15:52 +0300

    Промежуточный вариант лабораторной №6

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ void setup_LEDS () {
     //Настройка для сид
    rcc_periph_clock_enable(RCC_GPIOE);
    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LEDS);
-   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11 | GPIO13);
+   gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO11 | GPIO15);
 }
 
 void setup_timer () {
@@ -30,6 +30,21 @@ void setup_timer () {
     nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
 }
 
+void setup_timer_1 () {
+    rcc_periph_clock_enable(RCC_TIM1);
+
+    timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS - 1);
+    timer_set_period(TIM1, PERIOD_MS - 1);
+
+    timer_set_oc_value(TIM1, TIM_OC1, PERIOD_MS / 3);
+    timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
+
+    timer_enable_oc_output(TIM1, TIM_OC1);
+    timer_enable_break_main_output(TIM1);
+    
+    timer_enable_counter(TIM1);
+    
+}
 void blink_LEDS () {
         if (timer_get_counter(TIM6) < PERIOD_MS / 2)
             gpio_set(GPIOE, LEDS);
@@ -44,13 +59,17 @@ int main () {
     setup_LEDS();
     setup_timer();
 
+    //Для того, чтобы СИДом управлял таймер, необходимо настроить таймер и линию порта
+    setup_timer_1();
+    //setup_timer_port();
+
     while (true) {
-        blink_LEDS(); 
+        blink_LEDS();
     }
 }
 
 void tim6_dac_isr(){
     timer_clear_flag(TIM6, TIM_SR_UIF);
-    gpio_toggle(GPIOE, GPIO11 | GPIO13);
+    gpio_toggle(GPIOE, GPIO11 | GPIO15);
 
 }
