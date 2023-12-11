5532a3087390eb1081d754be9d612c20729fa940
Алексей Скворцов <nakamych@mail.ru>
2023-12-11 13:10:34 +0300

    Лаба№7

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 
 
 constexpr uint16_t LEDS{GPIO9|GPIO13|GPIO8|GPIO12};
-constexpr uint16_t PERIOD_MS{1000};
+constexpr uint16_t CNT_FREQ_HZ{1000};
+constexpr uint16_t PERIOD_MS{20};
 void setup_LEDS () {
     rcc_periph_clock_enable(RCC_GPIOE);
 
@@ -14,7 +15,7 @@ gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT,GPIO_PUPD_NONE, GPIO11|GPIO15 );
 }
 void setup_timer () {
     rcc_periph_clock_enable(RCC_TIM6);
-    timer_set_prescaler(TIM6,rcc_get_timer_clk_freq(TIM6)/PERIOD_MS-1);
+    timer_set_prescaler(TIM6,rcc_get_timer_clk_freq(TIM6)/CNT_FREQ_HZ-1 );
 timer_set_period(TIM6,PERIOD_MS-1 );
 timer_enable_irq(TIM6,TIM_DIER_UIE);
 nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
@@ -23,7 +24,7 @@ timer_enable_counter(TIM6);
 }
 void setup_timer_1 () {
     rcc_periph_clock_enable(RCC_TIM1);
-    timer_set_prescaler(TIM1,rcc_get_timer_clk_freq(TIM1)/PERIOD_MS-1);
+    timer_set_prescaler(TIM1,rcc_get_timer_clk_freq(TIM1)/CNT_FREQ_HZ-1 );
 timer_set_period(TIM1,PERIOD_MS-1 );
 timer_set_oc_value(TIM1,TIM_OC1,PERIOD_MS /3);
 timer_set_oc_mode(TIM1,TIM_OC1,TIM_OCM_PWM1);
@@ -32,13 +33,19 @@ timer_enable_break_main_output(TIM1);
 timer_enable_counter(TIM1);
 
 }
-// void blink_LEDS () {
-// if (timer_get_counter(TIM6)<PERIOD_MS/2)
-// gpio_set(GPIOE,LEDS);
-// else
-// gpio_clear(GPIOE,LEDS);
+void setup_timer_port(){
+     rcc_periph_clock_enable(RCC_GPIOE);
+gpio_mode_setup(GPIOE, GPIO_MODE_AF,GPIO_PUPD_NONE, GPIO9);
+gpio_set_af(GPIOE, GPIO_AF2, GPIO9);
+}
+void blink_LEDS () {
+if (timer_get_counter(TIM6)<PERIOD_MS/2)
+gpio_set(GPIOE,LEDS);
+else
+gpio_clear(GPIOE,LEDS);
     
-// }
+}
+
 
 int main () {
 rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
@@ -46,11 +53,11 @@ setup_LEDS();
 setup_timer();
 //для того, чтобы  СИДом управлял таймер,необходимо настроить таймер и линию порта
 setup_timer_1();
-// setup_timer_port();
+setup_timer_port();
 
+
+    while (true) { 
 // blink_LEDS();
-//     while (true) { 
-
 // gpio_set(GPIOE, GPIO9 | GPIO13 );
 // for(volatile uint32_t i=0;i<350'000; ++i);
 // gpio_clear(GPIOE,GPIO9 | GPIO13 );
@@ -58,10 +65,19 @@ setup_timer_1();
 // for(volatile uint32_t i=0;i<350'000; ++i);
 // gpio_clear(GPIOE,GPIO8 |GPIO12 );
   
-//    }
+   }
 }
 
 void tim6_dac_isr(){
     timer_clear_flag(TIM6,TIM_SR_UIF);
     gpio_toggle(GPIOE,GPIO11|GPIO15);
+    static uint8_t rep{0};
+    rep++;
+    rep %= 4;
+    if(rep==0){
+    static uint8_t rep2{0};
+    rep2++;
+    rep2 %= 5;
+    if(rep2==0) timer_set_oc_value(TIM1,TIM_OC1,PERIOD_MS /(rep2+1));
+    }
 }
