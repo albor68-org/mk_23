953dfc870b1e8db3cd151be7fc96302e00e78105
Алексей Балашов <balashovalex1029@gmail.com>
2023-12-11 13:10:29 +0300

    Lab7

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/timer.h>
 #include <libopencm3/cm3/nvic.h>
-constexpr uint16_t LEDS{GPIO9|GPIO13};
-constexpr uint16_t PERIOD_MS{1000};
+constexpr uint16_t LEDS{GPIO8|GPIO9|GPIO10|GPIO11|GPIO12|GPIO13|GPIO14|GPIO15};
+constexpr uint16_t CNT_FREQ_HZ{1000};
+constexpr uint16_t PERIOD_MS{30};
 
 void setup_LEDS (){
 rcc_periph_clock_enable(RCC_GPIOE);
@@ -15,7 +16,7 @@ gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,GPIO11|GPIO15);
 
 void setup_timer (){
     rcc_periph_clock_enable(RCC_TIM6);
-timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6)/PERIOD_MS-1);
+timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6)/CNT_FREQ_HZ-1);
 timer_set_period(TIM6, PERIOD_MS-1);
 timer_enable_irq(TIM6, TIM_DIER_UIE);
 nvic_enable_irq(NVIC_TIM6_DAC_IRQ);
@@ -24,13 +25,20 @@ timer_enable_counter(TIM6);
 
 void setup_timer_1 (){
     rcc_periph_clock_enable(RCC_TIM1);
-timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1)/PERIOD_MS-1);
+timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1)/CNT_FREQ_HZ-1);
 timer_set_period(TIM1, PERIOD_MS-1);
 timer_set_oc_value(TIM1, TIM_OC1, PERIOD_MS /3);
 timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
 timer_enable_oc_output(TIM1, TIM_OC1);
 timer_enable_break_main_output(TIM1);
 timer_enable_counter(TIM1);
+}
+
+void setup_timer_port () {
+    rcc_periph_clock_enable(RCC_GPIOE);
+gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
+gpio_set_af(GPIOE, GPIO_AF2, GPIO9);
+
 }
 
 void blink_LEDS (){
@@ -43,12 +51,12 @@ gpio_clear(GPIOE,LEDS);
 int main () {
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]);
 setup_LEDS ();
-blink_LEDS ();
+// blink_LEDS ();
 setup_timer ();
 setup_timer_1 ();
-// setup_timer_port ();
+ setup_timer_port ();
 
-while (true) {
+ while (true) {
     blink_LEDS();
     //    gpio_set(GPIOE, GPIO8|GPIO12);
     //    for(volatile uint32_t i=0;i<300'000;++i);
@@ -62,10 +70,13 @@ while (true) {
     //           gpio_set(GPIOE, GPIO11|GPIO15);
     //    for(volatile uint32_t i=0;i<300'000;++i);
     //    gpio_clear(GPIOE, GPIO11|GPIO15);
-    }
-}
+ }
+     }
 void tim6_dac_isr () {
     timer_clear_flag(TIM6, TIM_SR_UIF);
 gpio_toggle(GPIOE, GPIO11 | GPIO15);
-
+static uint8_t rep{0};
+rep++;
+rep %=4;
+if (rep == 0) timer_set_oc_value(TIM1, TIM_OC1, PERIOD_MS /(rep+2));
 }
