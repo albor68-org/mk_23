0dac136125824ec95615b3a6c5fe3174ce432b92
Арина Жучкова <liana5818an@gmail.com>
2023-12-18 13:56:07 +0300

    ЛБ7

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 #include <libopencm3/cm3/nvic.h>
 
 constexpr uint16_t LEDS{GPIO9 | GPIO13};
-constexpr uint16_t PERIOD_MS{1000};
+constexpr uint16_t CNT_FREQ_HZ{1000};
+constexpr uint16_t PERIOD_MS{40};
 
 void setup_LEDS() {
     rcc_periph_clock_enable(RCC_GPIOE);
@@ -14,7 +15,7 @@ void setup_LEDS() {
 void setup_timer() {
     rcc_periph_clock_enable(RCC_TIM6);
 
-    timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6) / PERIOD_MS - 1);
+    timer_set_prescaler(TIM6, rcc_get_timer_clk_freq(TIM6) / CNT_FREQ_HZ - 1);
     timer_set_period(TIM6, PERIOD_MS - 1);
 
     timer_enable_irq(TIM6, TIM_DIER_UIE);// irq-interrupt request, uie-update interrupt enable
@@ -25,7 +26,7 @@ void setup_timer() {
 void setup_timer_1() {
     rcc_periph_clock_enable(RCC_TIM1);
 
-    timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / PERIOD_MS - 1);
+    timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / CNT_FREQ_HZ - 1);
     timer_set_period(TIM1, PERIOD_MS - 1);
 
     timer_set_oc_value(TIM1, TIM_OC1, PERIOD_MS / 3);
@@ -35,6 +36,7 @@ void setup_timer_1() {
     timer_enable_break_main_output(TIM1);
 
     timer_enable_counter(TIM1);}
+
 void setup_timer_port(){
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9);
@@ -53,7 +55,6 @@ int main () {
     setup_LEDS();
     setup_timer();
 
-//Чтобы СИ управлял таймер, необход. настроить его и линию порта
     setup_timer_1();
     setup_timer_port();
 
@@ -64,4 +65,14 @@ int main () {
 void tim6_dac_isr() {
     timer_clear_flag(TIM6, TIM_SR_UIF);
     gpio_toggle(GPIOE, GPIO11 | GPIO15);
+
+    static uint8_t rep{0};
+    rep++;
+    rep %= 4;
+    if (rep == 0){ 
+    static uint8_t rep2{0};
+    rep2++;
+    rep2 %=5;
+    timer_set_oc_value(TIM1, TIM_OC1, PERIOD_MS / (rep2 +2));
+}
 }
