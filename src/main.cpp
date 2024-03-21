1b59aeb22593d18e668f0f74235febcb91cd5371
Болдин Андрей <aboldin003@gmail.com>
2024-03-21 11:51:05 +0300

    001

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
 
+uint8_t c;
 void setup () {
 
     rcc_periph_clock_enable(RCC_GPIOA);
@@ -15,21 +17,44 @@ void setup () {
     usart_set_parity(USART2, USART_PARITY_NONE);
     usart_set_mode(USART2, USART_MODE_TX_RX);
     usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+usart_enable(USART2);
+
+    usart_enable_rx_interrupt(USART2);
+    nvic_enable_irq(NVIC_USART2_EXTI26_IRQ);
+
     usart_enable(USART2);
 
+    rcc_periph_clock_enable(RCC_GPIOE);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9 | GPIO11 | GPIO15);
 }
 
 void loop () {
-    uint8_t c = usart_recv_blocking(USART2);
-    usart_send_blocking(USART2, c);
+    //uint8_t c = usart_recv_blocking(USART2);
+
+    usart_send_blocking(USART2, 'c');
+    for (volatile uint32_t i=0; i<200000; ++i);
+    gpio_toggle(GPIOE, GPIO9);
 }
 
+//---------------------------------------------------------------------------------------------
 int main () {
 
-    setup();
+    setup(); //После настройки можно подать сигнал - включить светодиод PE9 (порт должен быть настроен)
+    gpio_set(GPIOE, GPIO15);
 
     while (true) {
         loop();
     }
 }
+//---------------------------------------------------------------------------------------------
+void usart2_exti26_isr (void) {
 
+    USART_ISR(USART2) &= ~(USART_ISR_RXNE);
+    //[[maybe_unused]]
+    c = static_cast<uint8_t>(usart_recv(USART2));
+    gpio_toggle(GPIOE, GPIO11);
+    
+    // Очистить флаг запроса прерывания
+    //Сохранить принятый символ в переменную (глобальную, надо определить!)
+    //Переключить светодиод, например, PE11 (настроить порт для работы со светодиодом, в setup!)
+}
