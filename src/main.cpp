dbbdbfac3a81d49549984356e2f9a19099befa05
Способова Юлия <sportlove0018@gmail.com>
2024-03-21 13:03:49 +0300

    povtor bukvu

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,44 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/timer.h>
-#include <libopencm3/cm3/nvic.h>
- constexpr uint16_t PERIOD_MS{1000};
+#include <libopencm3/stm32/usart.h>
 
- void LED_gpio_setup(){
-    rcc_periph_clock_enable(RCC_GPIOE);
-gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO9  | GPIO11 );
-gpio_mode_setup(GPIOE, GPIO_MODE_AF, GPIO_PUPD_NONE,GPIO14);
-gpio_set_af(GPIOE,GPIO_AF2,GPIO14);
+void setup (){
+    // Интерфейс U(S)ART с внешним миром
+rcc_periph_clock_enable(RCC_GPIOA);                           // Разморозка порта ввода/вывода
+
+gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO9 | GPIO10);  // Режим альтернативной функции
+gpio_set_af(GPIOA,GPIO_AF7, GPIO9 | GPIO10);                           // Альтернативная функция (выбор по номеру) PA9 --- Tx, PA10 --- Rx.
+
+rcc_periph_clock_enable(RCC_USART1);                      // Разморозка ПУ
+
+usart_set_baudrate(USART1, 115200);                       // Скорость передачи
+usart_set_databits(USART1, 8);                            // Размер посылки
+usart_set_stopbits(USART1, USART_STOPBITS_1);             // Количество стоп-битов
+usart_set_parity(USART1, USART_PARITY_NONE);              // Контроль четности
+
+usart_set_mode(USART1, USART_MODE_TX_RX);                 // Режим работы ПУ
+usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);   // Управление процессом передачи сообщений
+
+usart_enable_rx_interrupt(USART1);
+usart_enable_irq(NVIC_USART1_EXTI25_IRQ);
+
+usart_enable(USART1);                                     // Включение ПУ
+
 }
 
-void timer_setup(){
-    rcc_periph_clock_enable(RCC_TIM1);
-    timer_set_prescaler(TIM1, rcc_get_timer_clk_freq(TIM1) / (PERIOD_MS - 1));
-    timer_set_period(TIM1,  PERIOD_MS - 1);
-    timer_enable_irq(TIM1, TIM_DIER_UIE);
-    nvic_enable_irq(NVIC_TIM1_UP_TIM16_IRQ);
-    timer_enable_counter(TIM1);
-    }
-
-
-void blink_LED(){
-    if (timer_get_counter(TIM1) < (PERIOD_MS / 2)) gpio_set(GPIOE, GPIO9) ;
-    else gpio_clear(GPIOE, GPIO9);
+void loop(){
+usart_send_blocking (USART1 , 0x55);
+for (volatile uint32_t)
 }
 int main (){
-//Настройка тактовой подсистемы
-rcc_clock_setup_pll(
-    &rcc_hsi_configs[RCC_CLOCK_HSI_64MHZ]
-);
- 
+    setup();
+   while(true){
+    loop();
 
-   LED_gpio_setup();
-   timer_setup();
-    while (true) {
-blink_LED();
-    }
+   } 
 }
-void tim1_up_tim16_isr(void){
-    timer_clear_flag(TIM1, TIM_SR_UIF);
-    gpio_toggle(GPIOE, GPIO11);
-}
+ void usart1_isr(void){
+    //очистить флаг запроса прерывания
+    //сохранить принятый символ в переменную (глобальную надо определить!)
+    //переключить светодиод
+ }
